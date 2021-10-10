b671998a3fb7a1e20c66c4265924c107117cc646
Sanketh Varamballi <sankethv03@gmail.com>
2021-10-10 17:51:44 -0400

    added command line arguments

--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@
 #include "include/lexer.hpp"
 using namespace thimble;
 
-int main() {
-  std::ifstream fin("test.txt", std::ifstream::in);
-  Lexer l(fin);
-  Token tok;
-  do {
-    tok = l.get_next_token();
-  } while (tok.type != T_EOF);
+int main(int argc, char *argv[]) {
+  if (argc == 2) {
+    std::ifstream fin(argv[1]);
+    Lexer l(fin);
+    Token tok;
+    do {
+      tok = l.get_next_token();
+    } while (tok.type != T_EOF);
+
+    return 0;
+  } else {
+    std::cerr << "Usage: " << argv[0] << " <filename>" << std::endl;
+    return 1;
+  }
   return 0;
 }
